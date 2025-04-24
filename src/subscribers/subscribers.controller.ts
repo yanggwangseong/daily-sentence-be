6d7089432eea4923382bcd2a19eb4f8dd2969066
YangGwangSeong <rhkdtjd_12@naver.com>
2025-04-24 14:51:57 +0900

    feat: email-validation-pipe 추가 (#1)

--- src/subscribers/subscribers.controller.ts
+++ src/subscribers/subscribers.controller.ts
@@ -1,12 +1,13 @@
 import { ConflictException, Controller, Param, Post } from '@nestjs/common';
 import { SubscribersService } from './subscribers.service';
+import { EmailPipe } from './pipes/email.pipe';
 
 @Controller('subscribers')
 export class SubscribersController {
 	constructor(private readonly subscribersService: SubscribersService) {}
 
 	@Post(':email')
-	async create(@Param('email') email: string) {
+	async create(@Param('email', new EmailPipe()) email: string) {
 		const subscriber = await this.subscribersService.create(email);
 
 		if (subscriber) throw new ConflictException('이미 구독자입니다.');
