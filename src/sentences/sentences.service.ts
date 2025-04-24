2dd117c025715dbb88dfbcd048bb94200c58489f
YangGwangSeong <rhkdtjd_12@naver.com>
2025-04-24 18:14:38 +0900

    feat: 1주일 월요일부터 가져오기 (#1)

--- src/sentences/sentences.service.ts
+++ src/sentences/sentences.service.ts
@@ -31,10 +31,17 @@ export class SentencesService {
 	async getWeeklySentences(date: string) {
 		const inputDate = new Date(date);
 
+		// 월요일을 주의 시작으로 계산 (1: 월요일, ..., 0: 일요일)
+		const day = inputDate.getDay();
+		const diff = day === 0 ? 6 : day - 1; // 일요일(0)이면 6을 빼고, 아니면 요일-1을 빼서 월요일로 맞춤
+
+		// 월요일(시작일)
 		const startDate = new Date(inputDate);
-		startDate.setDate(inputDate.getDate() - inputDate.getDay());
-		const endDate = new Date(inputDate);
-		endDate.setDate(inputDate.getDate() - inputDate.getDay() + 6);
+		startDate.setDate(inputDate.getDate() - diff);
+
+		// 일요일(종료일)
+		const endDate = new Date(startDate);
+		endDate.setDate(startDate.getDate() + 6);
 
 		const startDateStr = startDate.toLocaleDateString('sv-SE', {
 			timeZone: 'Asia/Seoul',
