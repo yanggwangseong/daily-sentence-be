3968f7fb172ad5a8ad3053523e0d7337d2021b74
YangGwangSeong <rhkdtjd_12@naver.com>
2025-05-09 15:04:21 +0900

    test: sentence repository 유닛 테스트 추가 (#2)

--- src/sentences/repositories/sentences.repository.ts
+++ src/sentences/repositories/sentences.repository.ts
@@ -1,35 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-import { SentencesEntity } from '../entities/sentences.entity';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { SentencesEntity } from "../entities/sentences.entity";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class SentencesRepository extends Repository<SentencesEntity> {
-	constructor(
-		@InjectRepository(SentencesEntity)
-		private readonly repository: Repository<SentencesEntity>,
-	) {
-		super(repository.target, repository.manager, repository.queryRunner);
-	}
+    constructor(
+        @InjectRepository(SentencesEntity)
+        private readonly repository: Repository<SentencesEntity>,
+    ) {
+        super(repository.target, repository.manager, repository.queryRunner);
+    }
 
-	findOneByDate(datetime: string) {
-		return this.repository
-			.createQueryBuilder('sentence')
-			.leftJoinAndSelect('sentence.vocabs', 'vocab')
-			.leftJoinAndSelect('sentence.video', 'video')
-			.where('DATE(sentence.createdAt) = :date', { date: datetime })
-			.getOne();
-	}
+    findOneByDate(datetime: string) {
+        return this.repository
+            .createQueryBuilder("sentence")
+            .leftJoinAndSelect("sentence.vocabs", "vocab")
+            .leftJoinAndSelect("sentence.video", "video")
+            .where("DATE(sentence.createdAt) = :date", { date: datetime })
+            .getOne();
+    }
 
-	findByDateRange(startDate: string, endDate: string) {
-		return this.repository
-			.createQueryBuilder('sentence')
-			.leftJoinAndSelect('sentence.vocabs', 'vocab')
-			.leftJoinAndSelect('sentence.video', 'video')
-			.where('DATE(sentence.createdAt) BETWEEN :startDate AND :endDate', {
-				startDate,
-				endDate,
-			})
-			.getMany();
-	}
+    findByDateRange(startDate: string, endDate: string) {
+        return this.repository
+            .createQueryBuilder("sentence")
+            .leftJoinAndSelect("sentence.vocabs", "vocab")
+            .leftJoinAndSelect("sentence.video", "video")
+            .where("DATE(sentence.createdAt) BETWEEN :startDate AND :endDate", {
+                startDate,
+                endDate,
+            })
+            .getMany();
+    }
 }
